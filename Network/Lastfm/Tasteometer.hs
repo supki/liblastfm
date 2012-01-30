ea5dc692dedcd50ca235de9c74a0d12cbfd1d2c9
Dmitry Malikov <color404051h@gmail.com>
2012-01-31 01:17:09 +0400

    Tasteometer: add datatypes

--- Network/Lastfm/Tasteometer.hs
+++ Network/Lastfm/Tasteometer.hs
@@ -1,24 +1,36 @@
 module Network.Lastfm.Tasteometer
-  ( getScore
-  , getSimilarArtists
+  ( compare
   ) where
 
 import Prelude hiding (compare)
 import Control.Applicative ((<$>))
+import Data.List (intersperse)
+import Text.XML.Light
 import Network.Lastfm.Core
 
-type Username = String
 type APIKey = String
+type Response = [Element]
 
-compare apiKey username1 username2 = callAPI
+type Type = String                          -- ^ "user" | "artists"
+data Value = User String | Artists [String] -- ^ [Last.fm username] | [Comma-separated artist names (max. 100)]
+type Limit = Double                         -- ^ How many shared artists to display
+
+compare :: APIKey -> Type -> Value -> Type -> Value -> Limit -> IO Response
+compare apiKey type1 value1 type2 value2 limit = callAPI
   [ ("method", "tasteometer.compare")
-  , ("type1", "user"), ("value1", username1)
-  , ("type2", "user"), ("value2", username2)
-  , ("limit", "10")
+  , ("type1", type1), ("value1", handleValue value1)
+  , ("type2", type2), ("value2", handleValue value2)
+  , ("limit", show limit)
   , ("api_key", apiKey) ]
+    where
+        handleValue :: Value -> String
+        handleValue (User user) = user
+        handleValue (Artists artists) = concat . intersperse "," $ artists
 
-getScore :: APIKey -> Username -> Username -> IO (Maybe String)
-getScore apiKey username1 username2 = firstInnerTagContent "score" <$> compare apiKey username1 username2
+getScore :: APIKey -> Value -> Value -> IO (Maybe String)
+getScore apiKey username1 username2 =
+    firstInnerTagContent "score" <$> compare apiKey "user" username1 "user" username2 10
 
-getSimilarArtists :: APIKey -> Username -> Username -> IO [String]
-getSimilarArtists apiKey username1 username2 = allInnerTagsContent "name" <$> getAllInnerTags "artist" <$> compare apiKey username1 username2
+getSimilarArtists :: APIKey -> Value -> Value -> IO [String]
+getSimilarArtists apiKey username1 username2 =
+    allInnerTagsContent "name" <$> getAllInnerTags "artist" <$> compare apiKey "user" username1 "user" username2 10
