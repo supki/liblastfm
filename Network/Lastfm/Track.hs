4b4d62c169870313f0eb155167c0e095531f0c49
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-01-30 04:51:00 +0400

    Add love/unlove calls.

--- Network/Lastfm/Track.hs
+++ Network/Lastfm/Track.hs
@@ -1,1 +1,28 @@
-module Network.Lastfm.Track where
+module Network.Lastfm.Track
+  ( love, unlove
+  ) where
+
+import Network.Lastfm.Core
+
+type APIKey = String
+type Artist = String
+type Track = String
+type SessionKey = String
+
+love :: Track -> Artist -> APIKey -> SessionKey -> IO ()
+love track artist apiKey sessionKey = callAPI
+  [ ("method","track.love")
+  , ("track", track)
+  , ("artist", artist)
+  , ("api_key", apiKey)
+  , ("sk", sessionKey)
+  ] >> return ()
+
+unlove :: Track -> Artist -> APIKey -> SessionKey -> IO ()
+unlove track artist apiKey sessionKey = callAPI
+  [ ("method","track.unlove")
+  , ("track", track)
+  , ("artist", artist)
+  , ("api_key", apiKey)
+  , ("sk", sessionKey)
+  ] >> return ()
