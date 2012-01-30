d4aaeb59ee669b8247147869517da34259e4be8f
Dmitry Malikov <color404051h@gmail.com>
2012-01-30 10:45:45 +0400

    Tasteometer: cosmetics

--- Network/Lastfm/Tasteometer.hs
+++ Network/Lastfm/Tasteometer.hs
@@ -9,16 +9,15 @@ import Network.Lastfm.Core
 type Username = String
 type APIKey = String
 
-getScore :: APIKey -> Username -> Username -> IO (Maybe String)
-getScore apiKey username1 username2 = tagContent "score" <$> callAPI
+callAPITasteometer apiKey username1 username2 = callAPI
   [ ("method", "tasteometer.compare")
   , ("type1", "user"), ("value1", username1)
   , ("type2", "user"), ("value2", username2)
+  , ("limit", "100")
   , ("api_key", apiKey) ]
 
+getScore :: APIKey -> Username -> Username -> IO (Maybe String)
+getScore apiKey username1 username2 = tagContent "score" <$> callAPITasteometer apiKey username1 username2
+
 getSimilarArtists :: APIKey -> Username -> Username -> IO [String]
-getSimilarArtists apiKey username1 username2 = (init . init . tagContents "name") <$> callAPI
-  [ ("method", "tasteometer.compare")
-  , ("type1", "user"), ("value1", username1)
-  , ("type2", "user"), ("value2", username2)
-  , ("api_key", apiKey) ]
+getSimilarArtists apiKey username1 username2 = (init . init . tagContents "name") <$> callAPITasteometer apiKey username1 username2
