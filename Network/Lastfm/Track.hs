5ceb9705f0a51f0ae43f8af4c0153aadd9ebf48b
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-01-30 05:04:43 +0400

    Add updateNowPlaying call.

--- Network/Lastfm/Track.hs
+++ Network/Lastfm/Track.hs
@@ -1,6 +1,9 @@
 module Network.Lastfm.Track
   ( love, unlove
+  , updateNowPlaying
   ) where
+
+import Data.Maybe (fromMaybe)
 
 import Network.Lastfm.Core
 
@@ -8,6 +11,12 @@ type APIKey = String
 type Artist = String
 type Track = String
 type SessionKey = String
+type Album = String
+type AlbumArtist = String
+type Context = String
+type TrackNumber = String
+type Mbid = String
+type Duration = String
 
 love :: Track -> Artist -> APIKey -> SessionKey -> IO ()
 love track artist apiKey sessionKey = callAPI
@@ -26,3 +35,34 @@ unlove track artist apiKey sessionKey = callAPI
   , ("api_key", apiKey)
   , ("sk", sessionKey)
   ] >> return ()
+
+updateNowPlaying :: Track
+                 -> Artist
+                 -> Maybe Album
+                 -> Maybe AlbumArtist
+                 -> Maybe Context
+                 -> Maybe TrackNumber
+                 -> Maybe Mbid
+                 -> Maybe Duration
+                 -> APIKey
+                 -> SessionKey
+                 -> IO ()
+updateNowPlaying track artist album albumArtist context trackNumber mbid duration apiKey sessionKey = callAPI
+  ( [ ("method","track.updateNowPlaying")
+    , ("track", track)
+    , ("artist", artist)
+    , ("api_key", apiKey)
+    , ("sk", sessionKey)
+    ] ++
+    optional "album" album ++
+    optional "albumArtist" albumArtist ++
+    optional "context" context ++
+    optional "trackNumber" trackNumber ++
+    optional "mbid" mbid ++
+    optional "duration" duration ) >>
+  return ()
+
+optional :: String -> Maybe a -> [(String, a)]
+optional key value = case value of
+                       Just v  -> [(key, v)]
+                       Nothing -> []
