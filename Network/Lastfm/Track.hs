b2e9045ac7255a7887994c9bca86982f5c8e6dcc
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-01-31 20:15:42 +0400

    Add track.search, track.share, track.addTags and track.removeTag methods.

--- Network/Lastfm/Track.hs
+++ Network/Lastfm/Track.hs
@@ -4,21 +4,30 @@ module Network.Lastfm.Track
   , scrobble, updateNowPlaying
   ) where
 
+import Data.List (intercalate)
+
 import Network.Lastfm.Core
 
 type APIKey = String
-type Artist = String
-type Track = String
 type SessionKey = String
+
 type Album = String
 type AlbumArtist = String
-type Context = String
-type TrackNumber = String
-type Timestamp = String
-type StreamId = String
-type Mbid = String
-type Duration = String
+type Artist = String
 type ChosenByUser = String
+type Context = String
+type Duration = String
+type Limit = String
+type Mbid = String
+type Message = String
+type Page = String
+type Public = String
+type Recipient = String
+type StreamId = String
+type Tag = String
+type Timestamp = String
+type Track = String
+type TrackNumber = String
 
 ban :: Track -> Artist -> APIKey -> SessionKey -> IO ()
 ban track artist apiKey sessionKey = callAPI_
@@ -104,6 +113,51 @@ scrobble xs apiKey sessionKey = mapM_ scrobbleTrack xs
           optional "context" context ++
           optional "trackNumber" trackNumber ++
           optional "mbid" mbid
+
+search :: Maybe Limit -> Maybe Page -> Track -> Maybe Artist -> APIKey -> IO Response
+search limit page track artist apiKey = callAPI $
+  [ ("method","track.search")
+  , ("track", track)
+  , ("api_key", apiKey)
+  ] ++
+  optional "limit" limit ++
+  optional "page" page ++
+  optional "artist" artist
+
+share :: Artist -> Track -> Maybe Public -> Maybe Message -> [Recipient] -> APIKey -> SessionKey -> IO ()
+share artist track public message recipients apiKey sessionKey = callAPI_ $
+  [ ("method","track.share")
+  , ("artist", artist)
+  , ("track", track)
+  , ("recipient", intercalate "," recipients)
+  , ("api_key", apiKey)
+  , ("sk", sessionKey)
+  ] ++
+  optional "public" public ++
+  optional "message" message
+
+addTags :: Artist -> Track -> [Tag] -> APIKey -> SessionKey -> IO ()
+addTags artist track tags apiKey sessionKey
+  | null tags        = error "Track.addTags: empty tag list."
+  | length tags > 10 = error "Track.addTags: tag list length has exceeded maximum."
+  | otherwise        = callAPI_ $
+    [ ("method","track.addTags")
+    , ("artist", artist)
+    , ("track", track)
+    , ("tags", intercalate "," tags)
+    , ("api_key", apiKey)
+    , ("sk", sessionKey)
+    ]
+
+removeTag :: Artist -> Track -> Tag -> APIKey -> SessionKey -> IO ()
+removeTag artist track tag apiKey sessionKey = callAPI_ $
+  [ ("method","track.removeTag")
+  , ("artist", artist)
+  , ("track", track)
+  , ("tag", tag)
+  , ("api_key", apiKey)
+  , ("sk", sessionKey)
+  ]
 
 optional :: String -> Maybe a -> [(String, a)]
 optional key value = case value of
