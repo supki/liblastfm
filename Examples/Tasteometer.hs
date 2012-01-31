91ab5196fa9e6b74e1c01658c2d25df350887210
Dmitry Malikov <color404051h@gmail.com>
2012-01-31 14:19:33 +0400

    Tasteometer: remove redundant methods to Examples

---
+++ Examples/Tasteometer.hs
@@ -0,0 +1,14 @@
+import Network.Lastfm.Core
+import Network.Lastfm.Tasteometer
+import Control.Applicative ((<$>))
+import Prelude hiding (compare)
+
+type APIKey = String
+
+getScore :: APIKey -> Value -> Value -> IO (Maybe String)
+getScore apiKey username1 username2 =
+  firstInnerTagContent "score" <$> compare apiKey username1 username2 (Just 10)
+
+getSimilarArtists :: APIKey -> Value -> Value -> IO [String]
+getSimilarArtists apiKey username1 username2 =
+  allInnerTagsContent "name" <$> getAllInnerTags "artist" <$> compare apiKey username1 username2 (Just 10)
