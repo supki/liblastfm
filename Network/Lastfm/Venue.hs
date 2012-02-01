3f4f6652b5a6c6ce56dca2afecd4a8d86f67343a
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-02-01 13:04:05 +0400

    Implement Venue module.

--- Network/Lastfm/Venue.hs
+++ Network/Lastfm/Venue.hs
@@ -1,1 +1,42 @@
-module Network.Lastfm.Venue where
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module Network.Lastfm.Venue
+  ( Venue(..), FestivalsOnly(..), Page(..), Limit(..), Country(..)
+  , getEvents, getPastEvents, search
+  ) where
+
+import Network.Lastfm.Auth (APIKey)
+import Network.Lastfm.Core
+
+newtype Venue = Venue String deriving (Show, LastfmValue)
+newtype FestivalsOnly = FestivalsOnly Bool deriving (Show, LastfmValue)
+newtype Page = Page Int deriving (Show, LastfmValue)
+newtype Limit = Limit Int deriving (Show, LastfmValue)
+newtype Country = Country String deriving (Show, LastfmValue)
+
+getEvents :: Venue -> Maybe FestivalsOnly -> APIKey -> IO Response
+getEvents venue festivalsOnly apiKey = callAPI "venue.getEvents" $
+  [ "venue" ?< venue
+  , "api_key" ?< apiKey
+  ] ++ optional
+    [ "festivalsonly" ?<< festivalsOnly
+    ]
+
+getPastEvents :: Venue -> Maybe FestivalsOnly -> Maybe Page -> Maybe Limit -> APIKey -> IO Response
+getPastEvents venue festivalsOnly page limit apiKey = callAPI "venue.getPastEvents" $
+  [ "venue" ?< venue
+  , "api_key" ?< apiKey
+  ] ++ optional
+    [ "festivalsonly" ?<< festivalsOnly
+    , "page" ?<< page
+    , "limit" ?<< limit
+    ]
+
+search :: Venue -> Maybe Page -> Maybe Limit -> Maybe Country -> APIKey -> IO Response
+search venue page limit country apiKey = callAPI "venue.search" $
+  [ "venue" ?< venue
+  , "api_key" ?< apiKey
+  ] ++ optional
+    [ "page" ?<< page
+    , "limit" ?<< limit
+    , "country" ?<< country
+    ]
