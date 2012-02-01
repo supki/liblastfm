b78547bd4e5406287e82c8c7c6141b4a40ca43bc
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-02-01 12:29:00 +0400

    Implement Playlist module.

--- Network/Lastfm/Playlist.hs
+++ Network/Lastfm/Playlist.hs
@@ -1,1 +1,32 @@
-module Network.Lastfm.Playlist where
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module Network.Lastfm.Playlist
+  ( PlaylistID(..), Artist
+  , addTrack, create
+  ) where
+
+import Network.Lastfm.Auth (APIKey, SessionKey)
+import Network.Lastfm.Core
+import Network.Lastfm.Track (Track)
+
+newtype Artist = Artist String deriving (Show, LastfmValue)
+newtype Description = Description String deriving (Show, LastfmValue)
+newtype PlaylistID = PlaylistID String deriving (Show, LastfmValue)
+newtype Title = Title String deriving (Show, LastfmValue)
+
+addTrack :: PlaylistID -> Track -> Artist -> APIKey -> SessionKey -> IO ()
+addTrack playlist track artist apiKey sessionKey = callAPI_ "playlist.addTrack"
+  [ "playlistID" ?< playlist
+  , "track" ?< track
+  , "artist" ?< artist
+  , "api_key" ?< apiKey
+  , "sk" ?< sessionKey
+  ]
+
+create :: Maybe Title -> Maybe Description -> APIKey -> SessionKey -> IO Response
+create title description apiKey sessionKey = callAPI "playlist.create" $
+  [ "api_key" ?< apiKey
+  , "sk" ?< sessionKey
+  ] ++ optional
+    [ "title" ?<< title
+    , "description" ?<< description
+    ]
