7f3943bc95eb2ca713a86e46924b74d30ae1e973
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-02-03 13:15:30 +0400

    Implement Chart module.

--- Network/Lastfm/API/Chart.hs
+++ Network/Lastfm/API/Chart.hs
@@ -1,1 +1,32 @@
-module Network.Lastfm.API.Chart where
+module Network.Lastfm.API.Chart
+  ( getHypedArtists, getHypedTracks, getLovedTracks
+  , getTopArtists, getTopTags, getTopTracks
+  ) where
+
+import Network.Lastfm.Core
+import Network.Lastfm.Types ((?<), APIKey, Limit, Page)
+
+getHypedArtists :: Maybe Page -> Maybe Limit -> APIKey -> Lastfm Response
+getHypedArtists = get "getHypedArtists"
+
+getHypedTracks :: Maybe Page -> Maybe Limit -> APIKey -> Lastfm Response
+getHypedTracks = get "getHypedTracks"
+
+getLovedTracks :: Maybe Page -> Maybe Limit -> APIKey -> Lastfm Response
+getLovedTracks = get "getLovedTracks"
+
+getTopArtists :: Maybe Page -> Maybe Limit -> APIKey -> Lastfm Response
+getTopArtists = get "getTopArtists"
+
+getTopTags :: Maybe Page -> Maybe Limit -> APIKey -> Lastfm Response
+getTopTags = get "getTopTags"
+
+getTopTracks :: Maybe Page -> Maybe Limit -> APIKey -> Lastfm Response
+getTopTracks = get "getTopTracks"
+
+get :: String -> Maybe Page -> Maybe Limit -> APIKey -> Lastfm Response
+get method page limit apiKey = dispatch $ callAPI method
+  [ "page" ?< page
+  , "limit" ?< limit
+  , "api_key" ?< apiKey
+  ]
