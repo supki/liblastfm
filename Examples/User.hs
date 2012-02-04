dc3c35725add5f34279f482f1c395261f97a089d
Dmitry Malikov <color404051h@gmail.com>
2012-02-05 00:53:03 +0400

    Examples User: fix some, add some

--- Examples/User.hs
+++ Examples/User.hs
@@ -1,9 +1,27 @@
 import Network.Lastfm.Core (allInnerTagsContent, firstInnerTagContent)
-import Network.Lastfm.Types (APIKey(..), Limit(..), User(..))
+import Network.Lastfm.Types
 import Network.Lastfm.API.User
 
+import Data.List (intercalate)
 import Data.List.Split (splitEvery)
 
+-- getArtistTracks
+getArtistTracksExample :: Int -> User -> Artist -> APIKey -> IO ()
+getArtistTracksExample n user artist apiKey = do
+  response <- getArtistTracks user artist Nothing Nothing Nothing apiKey
+  case response of
+    Left e -> print e
+    Right r -> mapM_ putStrLn . take n . allInnerTagsContent "name" $ r
+
+-- getBannedTracks
+getBannedArtists :: User -> Limit -> APIKey -> IO ()
+getBannedArtists user limit apiKey = do
+  response <- getBannedTracks user (Just limit) Nothing apiKey
+  case response of
+    Left e -> print e
+    Right r -> mapM_ putStrLn . allInnerTagsContent "name" $ r
+
+-- getInfo
 getPlayCount :: User -> APIKey -> IO ()
 getPlayCount user apiKey = do
   response <- getInfo (Just user) apiKey
@@ -14,17 +32,19 @@ getPlayCount user apiKey = do
         printPlayCount Nothing = putStrLn ""
         printPlayCount (Just a) = putStrLn a
 
+-- getLovedTracks
 getAllLovedTracks :: User -> Limit -> APIKey -> IO ()
 getAllLovedTracks user limit apiKey = do
   response <- getLovedTracks user (Just limit) Nothing apiKey
-  case response of 
+  case response of
     Left e -> print e
-    Right r -> mapM_ (putStrLn . (\[a,b] -> b ++ " - " ++ a )) . splitEvery 2 . allInnerTagsContent "name" $ r
+    Right r -> mapM_ (putStrLn . intercalate " - ") . splitEvery 2 . allInnerTagsContent "name" $ r
 
 main = do
   let apiKey = APIKey "b25b959554ed76058ac220b7b2e0a026"
   let user = User "smpcln"
-  putStrLn "Play count:"
-  getPlayCount user apiKey
-  putStrLn "Last 10 loved tracks:"
-  getAllLovedTracks user (Limit 10) apiKey
+  let artist = Artist "Dvar"
+  putStrLn "\nPlay count:"; getPlayCount user apiKey
+  putStrLn "\nLast 10 loved tracks:"; getAllLovedTracks user (Limit 10) apiKey
+  putStrLn $ "\nSome " ++ show artist ++ "s tracks:"; getArtistTracksExample 10 user artist apiKey
+  putStrLn "\nBanned artists:"; getBannedArtists user (Limit 10) apiKey
