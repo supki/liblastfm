51b12c2fa42b8618a7fde16ae3d5629b8df47c38
Dmitry Malikov <color404051h@gmail.com>
2012-02-06 01:50:28 +0400

    Examples: cleanup

--- Examples/Tasteometer.hs
+++ Examples/Tasteometer.hs
@@ -3,32 +3,16 @@
 import Control.Monad ((<=<))
 import Prelude hiding (compare)
 
-import Network.Lastfm.API.Tasteometer
+import qualified Network.Lastfm.API.Tasteometer as Tasteometer
 import Network.Lastfm.Core
 import Network.Lastfm.Types
 
-apiKey = APIKey "b25b959554ed76058ac220b7b2e0a026"
-user1 = ValueUser . User $ "smpcln"
-user2 = ValueUser . User $ "ingolfr"
-
--- compare example
-getScore :: IO ()
-getScore = do response <- compare user1 user2 (Just . Limit $ 10) apiKey
-              putStr "Score: "
-              case response of
-                Left e -> print e
-                Right r -> print $ score r
+compare :: IO ()
+compare = do response <- Tasteometer.compare (ValueUser $ User "smpcln") (ValueUser $ User "ingolfr") (Just $ Limit 10) (APIKey "b25b959554ed76058ac220b7b2e0a026")
+             putStr "Score: "
+             case response of
+               Left e -> print e
+               Right r -> print $ score r
   where score = getContent <=< lookupChild "score" <=< lookupChild "result" <=< lookupChild "comparison"
 
--- compare example
-getSimilarArtists :: IO ()
-getSimilarArtists = do response <- compare user1 user2 (Just . Limit $ 10) apiKey
-                       putStr "Similar groups: "
-                       case response of
-                         Left e -> print e
-                         Right r -> print $ similarArtists r
-  where similarArtists = mapM (getContent <=< lookupChild "name") <=< lookupChildren "artist" <=< lookupChild "artists" <=< lookupChild "result" <=< lookupChild "comparison"
-
-main = do
-  getScore
-  getSimilarArtists
+main = compare
--- Examples/User.hs
+++ Examples/User.hs
@@ -6,71 +6,65 @@ import Control.Monad ((<=<))
 
 import Network.Lastfm.Core
 import Network.Lastfm.Types
-import Network.Lastfm.API.User
+import qualified Network.Lastfm.API.User as User
 
 apiKey = APIKey "b25b959554ed76058ac220b7b2e0a026"
 user1 = User "smpcln"
 user2 = User "mokele"
 artist = Artist "Dvar"
 
--- getArtistTracks example
-getArtistTracksExample :: IO ()
-getArtistTracksExample = do response <- getArtistTracks user1 artist Nothing Nothing Nothing apiKey
-                            putStr "Artist tracks: "
-                            case response of
-                              Left e -> print e
-                              Right r -> print $ artistTracks r
+getArtistTracks :: IO ()
+getArtistTracks = do response <- User.getArtistTracks user1 artist Nothing Nothing Nothing apiKey
+                     putStr "Artist tracks: "
+                     case response of
+                       Left e -> print e
+                       Right r -> print $ artistTracks r
   where artistTracks = mapM (getContent <=< lookupChild "name") <=< lookupChildren "track" <=< lookupChild "artisttracks"
 
--- getBannedTracks example
-getBannedArtists :: IO ()
-getBannedArtists = do response <- getBannedTracks user1 Nothing (Just $ Limit 10) apiKey
-                      putStr "Banned artists: "
-                      case response of
-                        Left e -> print e
-                        Right r -> print $ bannedArtists r
+getBannedTracks :: IO ()
+getBannedTracks = do response <- User.getBannedTracks user1 Nothing (Just $ Limit 10) apiKey
+                     putStr "Banned artists: "
+                     case response of
+                       Left e -> print e
+                       Right r -> print $ bannedArtists r
   where bannedArtists = mapM (getContent <=< lookupChild "name") <=< lookupChildren "track" <=< lookupChild "bannedtracks"
 
--- getEvents example
-getEventsExample :: IO ()
-getEventsExample = do response <- getEvents user2 Nothing (Just $ Limit 5) Nothing apiKey
-                      putStr "Events: "
-                      case response of
-                        Left e -> print e
-                        Right r -> print $ events r
+getEvents :: IO ()
+getEvents = do response <- User.getEvents user2 Nothing (Just $ Limit 5) Nothing apiKey
+               putStr "Events: "
+               case response of
+                 Left e -> print e
+                 Right r -> print $ events r
   where events = mapM (getContent <=< lookupChild "url" <=< lookupChild "venue") <=< lookupChildren "event" <=< lookupChild "events"
 
--- getFriends example
-getFriendsExample :: IO ()
-getFriendsExample = do response <- getFriends user1 Nothing Nothing (Just $ Limit 10) apiKey
-                       putStr "Friends: "
-                       case response of
-                         Left e -> print e
-                         Right r -> print $ friends r 
+getFriends :: IO ()
+getFriends = do response <- User.getFriends user1 Nothing Nothing (Just $ Limit 10) apiKey
+                putStr "Friends: "
+                case response of
+                  Left e -> print e
+                  Right r -> print $ friends r 
   where friends = mapM (getContent <=< lookupChild "name") <=< lookupChildren "user" <=< lookupChild "friends"
 
--- getInfo example
 getPlayCount :: IO ()
-getPlayCount = do response <- getInfo (Just user1) apiKey
+getPlayCount = do response <- User.getInfo (Just user1) apiKey
                   putStr "Play count: "
                   case response of
                     Left e -> print e
                     Right r -> print $ playCount r
   where playCount = getContent <=< lookupChild "playcount" <=< lookupChild "user"
 
--- getLovedTracks example
-getLovedTracksExample :: IO ()
-getLovedTracksExample = do response <- getLovedTracks user1 Nothing (Just $ Limit 10) apiKey
-                           putStr "Loved tracks: "
-                           case response of
-                             Left e -> print e
-                             Right r -> print $ lovedTracks r
+getLovedTracks :: IO ()
+getLovedTracks = do response <- User.getLovedTracks user1 Nothing (Just $ Limit 10) apiKey
+                    putStr "Loved tracks: "
+                    case response of
+                      Left e -> print e
+                      Right r -> print $ lovedTracks r
   where lovedTracks = mapM (getContent <=< lookupChild "name") <=< lookupChildren "track" <=< lookupChild "lovedtracks"
 
 main = do
-  getArtistTracksExample
-  getBannedArtists
-  getEventsExample
-  getFriendsExample
+  getArtistTracks
+  getBannedTracks
+  getEvents
+  getFriends
   getPlayCount
-  getLovedTracksExample
+  getLovedTracks
