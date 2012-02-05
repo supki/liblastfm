fdb625db0ee30a4e29a05609272c6287a4979544
Dmitry Malikov <color404051h@gmail.com>
2012-02-06 02:27:16 +0400

    Examples User: added some

--- Examples/User.hs
+++ Examples/User.hs
@@ -1,8 +1,6 @@
 #!/usr/bin/env runhaskell
 
 import Control.Monad ((<=<))
--- import Data.List (intercalate)
--- import Data.List.Split (splitEvery)
 
 import Network.Lastfm.Core
 import Network.Lastfm.Types
@@ -13,9 +11,11 @@ user1 = User "smpcln"
 user2 = User "mokele"
 artist = Artist "Dvar"
 
+pemis s = putStr $ "\n" ++ s
+
 getArtistTracks :: IO ()
 getArtistTracks = do response <- User.getArtistTracks user1 artist Nothing Nothing Nothing apiKey
-                     putStr "Artist tracks: "
+                     pemis "Artist tracks: "
                      case response of
                        Left e -> print e
                        Right r -> print $ artistTracks r
@@ -23,7 +23,7 @@ getArtistTracks = do response <- User.getArtistTracks user1 artist Nothing Nothi
 
 getBannedTracks :: IO ()
 getBannedTracks = do response <- User.getBannedTracks user1 Nothing (Just $ Limit 10) apiKey
-                     putStr "Banned artists: "
+                     pemis "Banned artists: "
                      case response of
                        Left e -> print e
                        Right r -> print $ bannedArtists r
@@ -31,7 +31,7 @@ getBannedTracks = do response <- User.getBannedTracks user1 Nothing (Just $ Limi
 
 getEvents :: IO ()
 getEvents = do response <- User.getEvents user2 Nothing (Just $ Limit 5) Nothing apiKey
-               putStr "Events: "
+               pemis "Events: "
                case response of
                  Left e -> print e
                  Right r -> print $ events r
@@ -39,15 +39,15 @@ getEvents = do response <- User.getEvents user2 Nothing (Just $ Limit 5) Nothing
 
 getFriends :: IO ()
 getFriends = do response <- User.getFriends user1 Nothing Nothing (Just $ Limit 10) apiKey
-                putStr "Friends: "
+                pemis "Friends: "
                 case response of
                   Left e -> print e
-                  Right r -> print $ friends r 
+                  Right r -> print $ friends r
   where friends = mapM (getContent <=< lookupChild "name") <=< lookupChildren "user" <=< lookupChild "friends"
 
 getPlayCount :: IO ()
 getPlayCount = do response <- User.getInfo (Just user1) apiKey
-                  putStr "Play count: "
+                  pemis "Play count: "
                   case response of
                     Left e -> print e
                     Right r -> print $ playCount r
@@ -55,11 +55,35 @@ getPlayCount = do response <- User.getInfo (Just user1) apiKey
 
 getLovedTracks :: IO ()
 getLovedTracks = do response <- User.getLovedTracks user1 Nothing (Just $ Limit 10) apiKey
-                    putStr "Loved tracks: "
+                    pemis "Loved tracks: "
                     case response of
                       Left e -> print e
                       Right r -> print $ lovedTracks r
   where lovedTracks = mapM (getContent <=< lookupChild "name") <=< lookupChildren "track" <=< lookupChild "lovedtracks"
+
+getNeighbours :: IO ()
+getNeighbours = do response <- User.getNeighbours user1 (Just $ Limit 10) apiKey
+                   pemis "Neighbours: "
+                   case response of
+                     Left e -> print e
+                     Right r -> print $ neighbours r
+  where neighbours = mapM (getContent <=< lookupChild "name") <=< lookupChildren "user" <=< lookupChild "neighbours"
+
+getNewReleases :: IO ()
+getNewReleases = do response <- User.getNewReleases user1 Nothing apiKey
+                    pemis "New releases: "
+                    case response of
+                      Left e -> print e
+                      Right r -> print $ newReleases r
+  where newReleases = mapM (getContent <=< lookupChild "url") <=< lookupChildren "album" <=< lookupChild "albums"
+
+getPastEvents :: IO ()
+getPastEvents = do response <- User.getPastEvents user2 Nothing (Just $ Limit 5) apiKey
+                   pemis "Past events: "
+                   case response of
+                     Left e -> print e
+                     Right r -> print $ pastEvents r
+  where pastEvents = mapM (getContent <=< lookupChild "url") <=< lookupChildren "event" <=< lookupChild "events"
 
 main = do
   getArtistTracks
@@ -68,3 +92,22 @@ main = do
   getFriends
   getPlayCount
   getLovedTracks
+  getNeighbours
+  getNewReleases
+  getPastEvents
+--  getPersonalTags
+--  getPlaylists
+--  getRecentStations
+--  getRecentTracks
+--  getRecommendedArtists
+--  getRecommendedEvents
+--  getShouts
+--  getTopAlbums
+--  getTopArtists
+--  getTopFans
+--  getTopTracks
+--  getWeeklyAlbumChart
+--  getWeeklyArtistChart
+--  getWeeklyChartList
+--  getWeeklyTrackChart
+--  shout
