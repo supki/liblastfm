a48fc3a0a636723504841d1d801f4ff9a70c6392
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-02-06 03:16:43 +0400

    Add stub for Playlist examples.

---
+++ Examples/Playlist.hs
@@ -0,0 +1,17 @@
+#!/usr/bin/env runhaskell
+
+import Control.Monad ((<=<))
+
+import Network.Lastfm.Core
+import Network.Lastfm.Types
+import qualified Network.Lastfm.API.Playlist as Playlist
+
+addTrack :: IO ()
+addTrack = undefined
+
+create :: IO ()
+create = undefined
+
+main :: IO ()
+main = do -- addTrack (requires authorization)
+          -- create (requires authorization)
