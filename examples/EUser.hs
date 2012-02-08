cb36cf815db68327abe48dc07403b41be45b9038
Dmitry Malikov <color404051h@gmail.com>
2012-02-09 00:50:48 +0400

    Examples User: fix case alignment

--- examples/EUser.hs
+++ examples/EUser.hs
@@ -14,7 +14,7 @@ getArtistTracks :: IO ()
 getArtistTracks = do response <- User.getArtistTracks (User "smpcln") (Artist "Dvar") Nothing Nothing Nothing apiKey
                      putStr "Artist tracks: "
                      case response of
-                       Left e -> print e
+                       Left e  -> print e
                        Right r -> print $ artistTracks r
                      putStrLn ""
   where artistTracks = mapM (getContent <=< lookupChild "name") <=< lookupChildren "track" <=< lookupChild "artisttracks" <=< wrap
@@ -23,7 +23,7 @@ getBannedTracks :: IO ()
 getBannedTracks = do response <- User.getBannedTracks (User "smpcln") Nothing (Just $ Limit 10) apiKey
                      putStr "Banned artists: "
                      case response of
-                       Left e -> print e
+                       Left e  -> print e
                        Right r -> print $ bannedArtists r
                      putStrLn ""
   where bannedArtists = mapM (getContent <=< lookupChild "name") <=< lookupChildren "track" <=< lookupChild "bannedtracks" <=< wrap
@@ -32,7 +32,7 @@ getEvents :: IO ()
 getEvents = do response <- User.getEvents (User "mokele") Nothing (Just $ Limit 5) Nothing apiKey
                putStr "Events: "
                case response of
-                 Left e -> print e
+                 Left e  -> print e
                  Right r -> print $ events r
                putStrLn ""
   where events = mapM (getContent <=< lookupChild "url" <=< lookupChild "venue") <=< lookupChildren "event" <=< lookupChild "events" <=< wrap
@@ -41,7 +41,7 @@ getFriends :: IO ()
 getFriends = do response <- User.getFriends (User "smpcln") Nothing Nothing (Just $ Limit 10) apiKey
                 putStr "Friends: "
                 case response of
-                  Left e -> print e
+                  Left e  -> print e
                   Right r -> print $ friends r
                 putStrLn ""
   where friends = mapM (getContent <=< lookupChild "name") <=< lookupChildren "user" <=< lookupChild "friends" <=< wrap
@@ -50,7 +50,7 @@ getPlayCount :: IO ()
 getPlayCount = do response <- User.getInfo (Just (User "smpcln")) apiKey
                   putStr "Play count: "
                   case response of
-                    Left e -> print e
+                    Left e  -> print e
                     Right r -> print $ playCount r
                   putStrLn ""
   where playCount = getContent <=< lookupChild "playcount" <=< lookupChild "user" <=< wrap
@@ -59,7 +59,7 @@ getLovedTracks :: IO ()
 getLovedTracks = do response <- User.getLovedTracks (User "smpcln") Nothing (Just $ Limit 10) apiKey
                     putStr "Loved tracks: "
                     case response of
-                      Left e -> print e
+                      Left e  -> print e
                       Right r -> print $ lovedTracks r
                     putStrLn ""
   where lovedTracks = mapM (getContent <=< lookupChild "name") <=< lookupChildren "track" <=< lookupChild "lovedtracks" <=< wrap
@@ -68,7 +68,7 @@ getNeighbours :: IO ()
 getNeighbours = do response <- User.getNeighbours (User "smpcln") (Just $ Limit 10) apiKey
                    putStr "Neighbours: "
                    case response of
-                     Left e -> print e
+                     Left e  -> print e
                      Right r -> print $ neighbours r
                    putStrLn ""
   where neighbours = mapM (getContent <=< lookupChild "name") <=< lookupChildren "user" <=< lookupChild "neighbours" <=< wrap
@@ -77,7 +77,7 @@ getNewReleases :: IO ()
 getNewReleases = do response <- User.getNewReleases (User "smpcln") Nothing apiKey
                     putStr "New releases: "
                     case response of
-                      Left e -> print e
+                      Left e  -> print e
                       Right r -> print $ newReleases r
                     putStrLn ""
   where newReleases = mapM (getContent <=< lookupChild "url") <=< lookupChildren "album" <=< lookupChild "albums" <=< wrap
@@ -86,7 +86,7 @@ getPastEvents :: IO ()
 getPastEvents = do response <- User.getPastEvents (User "mokele") Nothing (Just $ Limit 5) apiKey
                    putStr "Past events: "
                    case response of
-                     Left e -> print e
+                     Left e  -> print e
                      Right r -> print $ pastEvents r
                    putStrLn ""
   where pastEvents = mapM (getContent <=< lookupChild "url") <=< lookupChildren "event" <=< lookupChild "events" <=< wrap
@@ -95,7 +95,7 @@ getPersonalTags :: IO ()
 getPersonalTags = do response <- User.getPersonalTags (User "mokele") (Tag "rock") (TaggingType "artist") Nothing (Just $ Limit 10) apiKey
                      putStr "Personal tags: "
                      case response of
-                       Left e -> print e
+                       Left e  -> print e
                        Right r -> print $ personalTags r
                      putStrLn ""
   where personalTags = mapM (getContent <=< lookupChild "name") <=< lookupChildren "artist" <=< lookupChild "artists" <=< lookupChild "taggings" <=< wrap
@@ -104,7 +104,7 @@ getPlaylists :: IO ()
 getPlaylists = do response <- User.getPlaylists (User "mokele") apiKey
                   putStr "Playlists: "
                   case response of
-                    Left e -> print e
+                    Left e  -> print e
                     Right r -> print $ playlists r
                   putStrLn ""
   where playlists = mapM (getContent <=< lookupChild "title") <=< lookupChildren "playlist" <=< lookupChild "playlists" <=< wrap
@@ -113,7 +113,7 @@ getRecentStations :: APIKey -> SessionKey -> IO ()
 getRecentStations apiKey sessionKey = do response <- User.getRecentStations (User "liblastfm") Nothing (Just $ Limit 10) apiKey sessionKey
                                          putStr "Recent stations: "
                                          case response of
-                                           Left e -> print e
+                                           Left e  -> print e
                                            Right r -> print $ recentStations r
                                          putStrLn ""
   where recentStations = mapM (getContent <=< lookupChild "name") <=< lookupChildren "station" <=< lookupChild "recentstations" <=< wrap
@@ -122,7 +122,7 @@ getRecentTracks :: IO ()
 getRecentTracks = do response <- User.getRecentTracks (User "smpcln") Nothing (Just $ Limit 10) Nothing Nothing apiKey
                      putStr "Recent tracks: "
                      case response of
-                       Left e -> print e
+                       Left e  -> print e
                        Right r -> print $ recentTracks r
                      putStrLn ""
   where recentTracks = mapM (getContent <=< lookupChild "name") <=< lookupChildren "track" <=< lookupChild "recenttracks" <=< wrap
@@ -131,7 +131,7 @@ getRecommendedArtists :: APIKey -> SessionKey -> IO ()
 getRecommendedArtists apiKey sessionKey = do response <- User.getRecommendedArtists Nothing (Just $ Limit 10) apiKey sessionKey
                                              putStr "Recommended artists: "
                                              case response of
-                                               Left e -> print e
+                                               Left e  -> print e
                                                Right r -> print $ recommendedArtists r
                                              putStrLn ""
   where recommendedArtists = mapM (getContent <=< lookupChild "name") <=< lookupChildren "artist" <=< lookupChild "recommendations" <=< wrap
@@ -140,7 +140,7 @@ getRecommendedEvents :: APIKey -> SessionKey -> IO ()
 getRecommendedEvents apiKey sessionKey = do response <- User.getRecommendedEvents Nothing (Just $ Limit 10) apiKey sessionKey
                                             putStr "Recommended events: "
                                             case response of
-                                              Left e -> print e
+                                              Left e  -> print e
                                               Right r -> print $ recommendedEvents r
                                             putStrLn ""
   where recommendedEvents = mapM (getContent <=< lookupChild "url") <=< lookupChildren "event" <=< lookupChild "events" <=< wrap
@@ -149,7 +149,7 @@ getShouts :: IO ()
 getShouts = do response <- User.getShouts (User "smpcln") Nothing (Just $ Limit 1) apiKey
                putStr "Shouts: "
                case response of
-                 Left e -> print e
+                 Left e  -> print e
                  Right r -> print $ shouts r
                putStrLn ""
   where shouts = mapM (getContent <=< lookupChild "body") <=< lookupChildren "shout" <=< lookupChild "shouts" <=< wrap
@@ -158,7 +158,7 @@ getTopAlbums :: IO ()
 getTopAlbums = do response <- User.getTopAlbums (User "smpcln") Nothing Nothing (Just $ Limit 5) apiKey
                   putStr "Top albums: "
                   case response of
-                    Left e -> print e
+                    Left e  -> print e
                     Right r -> print $ topAlbums r
                   putStrLn ""
   where topAlbums = mapM (getContent <=< lookupChild "name" <=< lookupChild "artist") <=< lookupChildren "album" <=< lookupChild "topalbums" <=< wrap
@@ -167,7 +167,7 @@ getTopArtists :: IO ()
 getTopArtists = do response <- User.getTopArtists (User "smpcln") Nothing Nothing (Just $ Limit 5) apiKey
                    putStr "Top artists: "
                    case response of
-                     Left e -> print e
+                     Left e  -> print e
                      Right r -> print $ topArtists r
                    putStrLn ""
   where topArtists = mapM (getContent <=< lookupChild "name") <=< lookupChildren "artist" <=< lookupChild "topartists" <=< wrap
@@ -176,7 +176,7 @@ getTopTags :: IO ()
 getTopTags = do response <- User.getTopTags (User "smpcln") (Just $ Limit 10) apiKey
                 putStr "Top tags: "
                 case response of
-                  Left e -> print e
+                  Left e  -> print e
                   Right r -> print $ topTags r
                 putStrLn ""
   where topTags = mapM (getContent <=< lookupChild "name") <=< lookupChildren "tag" <=< lookupChild "toptags" <=< wrap
@@ -185,7 +185,7 @@ getTopTracks :: IO ()
 getTopTracks = do response <- User.getTopTracks (User "smpcln") Nothing Nothing (Just $ Limit 10) apiKey
                   putStr "Top tracks: "
                   case response of
-                    Left e -> print e
+                    Left e  -> print e
                     Right r -> print $ topTracks r
                   putStrLn ""
   where topTracks = mapM (getContent <=< lookupChild "url") <=< lookupChildren "track" <=< lookupChild "toptracks" <=< wrap
@@ -194,7 +194,7 @@ getWeeklyAlbumChart :: IO ()
 getWeeklyAlbumChart = do response <- User.getWeeklyAlbumChart (User "rj") Nothing Nothing apiKey
                          putStr "Weekly album chart: "
                          case response of
-                           Left e -> print e
+                           Left e  -> print e
                            Right r -> print $ weeklyAlbumChart r
                          putStrLn ""
   where weeklyAlbumChart = mapM (getContent <=< lookupChild "url") <=< lookupChildren "album" <=< lookupChild "weeklyalbumchart" <=< wrap
@@ -203,7 +203,7 @@ getWeeklyArtistChart :: IO ()
 getWeeklyArtistChart = do response <- User.getWeeklyArtistChart (User "rj") Nothing Nothing apiKey
                           putStr "Weekly artist chart: "
                           case response of
-                            Left e -> print e
+                            Left e  -> print e
                             Right r -> print $ weeklyArtistChart r
                           putStrLn ""
   where weeklyArtistChart = mapM (getContent <=< lookupChild "url") <=< lookupChildren "artist" <=< lookupChild "weeklyartistchart" <=< wrap
@@ -212,7 +212,7 @@ getWeeklyChartList :: IO ()
 getWeeklyChartList = do response <- User.getWeeklyChartList (User "rj") apiKey
                         putStr "Weekly chart list: "
                         case response of
-                          Left e -> print e
+                          Left e  -> print e
                           Right r -> print $ take 10 r
                         putStrLn ""
 
@@ -220,7 +220,7 @@ getWeeklyTrackChart :: IO ()
 getWeeklyTrackChart = do response <- User.getWeeklyTrackChart (User "rj") Nothing Nothing apiKey
                          putStr "Weekly track chart: "
                          case response of
-                           Left e -> print e
+                           Left e  -> print e
                            Right r -> print $ weeklyTrackChart r
                          putStrLn ""
   where weeklyTrackChart = mapM (getContent <=< lookupChild "url") <=< lookupChildren "track" <=< lookupChild "weeklytrackchart" <=< wrap
@@ -228,7 +228,7 @@ getWeeklyTrackChart = do response <- User.getWeeklyTrackChart (User "rj") Nothin
 shout :: APIKey -> SessionKey -> IO ()
 shout apiKey sessionKey = do response <- User.shout (User "liblastfm") (Message "test message") apiKey sessionKey
                              case response of
-                               Left e -> print e
+                               Left e   -> print e
                                Right () -> return ()
 
 start :: IO ()
