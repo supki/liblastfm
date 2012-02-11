e58bb78a3156af555ac6d141bcb91b6b3879b4a3
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-02-11 05:30:10 +0400

    Add some documentation to Album module.

--- Network/Lastfm/API/Album.hs
+++ Network/Lastfm/API/Album.hs
@@ -9,6 +9,8 @@ import Network.Lastfm.Response
 import Network.Lastfm.Types ( (?<), Album, APIKey, Artist, Autocorrect, Country, Language, Limit
                             , Mbid, Message, Page, Public, Recipient, SessionKey, Tag, User)
 
+-- | Tag an album using a list of user supplied tags.
+-- link: http://www.lastfm.ru/api/show/album.addTags
 addTags :: (Artist, Album) -> [Tag] -> APIKey -> SessionKey -> Lastfm ()
 addTags (artist, album) tags apiKey sessionKey = dispatch go
   where go
@@ -23,7 +25,8 @@ addTags (artist, album) tags apiKey sessionKey = dispatch go
           ]
           where method = "album.addTags"
 
-
+-- | Get a list of Buy Links for a particular Album. It is required that you supply either the artist and track params or the mbid param.
+-- link: http://www.lastfm.ru/api/show/album.getBuylinks
 getBuyLinks :: Either (Artist, Album) Mbid -> Maybe Autocorrect -> Country -> APIKey -> Lastfm Response
 getBuyLinks a autocorrect country apiKey = dispatch $ callAPI method $ target ++
   [ "autocorrect" ?< autocorrect
@@ -35,6 +38,8 @@ getBuyLinks a autocorrect country apiKey = dispatch $ callAPI method $ target ++
                    Left (artist, album) -> ["artist" ?< artist, "album" ?< album]
                    Right mbid           -> ["mbid" ?< mbid]
 
+-- | Get the metadata for an album on Last.fm using the album name or a musicbrainz id. See playlist.fetch on how to get the album playlist.
+-- link: http://www.lastfm.ru/api/show/album.getInfo
 getInfo :: Either (Artist, Album) Mbid -> Maybe Autocorrect -> Maybe Language -> Maybe User -> APIKey -> Lastfm Response
 getInfo a autocorrect lang username apiKey = dispatch $ callAPI method $ target ++
   [ "autocorrect" ?< autocorrect
@@ -47,6 +52,8 @@ getInfo a autocorrect lang username apiKey = dispatch $ callAPI method $ target 
                    Left (artist, album) -> ["artist" ?< artist, "album" ?< album]
                    Right mbid           -> ["mbid" ?< mbid]
 
+-- | Get shouts for this album.
+-- link: http://www.lastfm.ru/api/show/album.getShouts
 getShouts :: Either (Artist, Album) Mbid -> Maybe Autocorrect -> Maybe Page -> Maybe Limit -> APIKey -> Lastfm Response
 getShouts a autocorrect page limit apiKey = dispatch $ callAPI method $ target ++
   [ "autocorrect" ?< autocorrect
@@ -59,6 +66,8 @@ getShouts a autocorrect page limit apiKey = dispatch $ callAPI method $ target +
                    Left (artist, album) -> ["artist" ?< artist, "album" ?< album]
                    Right mbid           -> ["mbid" ?< mbid]
 
+-- | Get the tags applied by an individual user to an album on Last.fm.
+-- link: http://www.lastfm.ru/api/show/album.getTags
 getTags :: Either (Artist, Album) Mbid -> Maybe Autocorrect -> Either User SessionKey -> APIKey -> Lastfm Response
 getTags a autocorrect b apiKey = dispatch $ callAPI method $ target ++ auth ++
   [ "autocorrect" ?< autocorrect
@@ -72,6 +81,8 @@ getTags a autocorrect b apiKey = dispatch $ callAPI method $ target ++ auth ++
                  Left user        -> ["user" ?< user]
                  Right sessionKey -> ["sk" ?< sessionKey]
 
+-- | Get the top tags for an album on Last.fm, ordered by popularity.
+-- link: http://www.lastfm.ru/api/show/album.getTopTags
 getTopTags :: Either (Artist, Album) Mbid -> Maybe Autocorrect -> APIKey -> Lastfm Response
 getTopTags a autocorrect apiKey = dispatch $ callAPI method $ target ++
   [ "autocorrect" ?< autocorrect
@@ -82,6 +93,8 @@ getTopTags a autocorrect apiKey = dispatch $ callAPI method $ target ++
                    Left (artist, album) -> ["artist" ?< artist, "album" ?< album]
                    Right mbid           -> ["mbid" ?< mbid]
 
+-- | Remove a user's tag from an album.
+-- link: http://www.lastfm.ru/api/show/album.removeTag
 removeTag :: Artist -> Album -> Tag -> APIKey -> SessionKey -> Lastfm ()
 removeTag artist album tag apiKey sessionKey = dispatch $ callAPI_ "album.removeTag"
   [ "artist" ?< artist
@@ -91,6 +104,8 @@ removeTag artist album tag apiKey sessionKey = dispatch $ callAPI_ "album.remove
   , "sk" ?< sessionKey
   ]
 
+-- | Search for an album by name. Returns album matches sorted by relevance.
+-- link: http://www.lastfm.ru/api/show/album.search
 search :: Album -> Maybe Page -> Maybe Limit -> APIKey -> Lastfm Response
 search album page limit apiKey = dispatch $ callAPI "album.search"
   [ "album" ?< album
@@ -99,6 +114,8 @@ search album page limit apiKey = dispatch $ callAPI "album.search"
   , "api_key" ?< apiKey
   ]
 
+-- | Share an album with one or more Last.fm users or other friends.
+-- link: http://www.lastfm.ru/api/show/album.share
 share :: Artist -> Album -> [Recipient] -> Maybe Message -> Maybe Public -> APIKey -> SessionKey -> Lastfm ()
 share artist album recipients message public apiKey sessionKey = dispatch go
   where go
