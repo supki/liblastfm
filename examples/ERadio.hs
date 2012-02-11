44b2cb9d9ef6f40a0cb18fe58f95305e93699e70
Dmitry Malikov <color404051h@gmail.com>
2012-02-11 15:35:13 +0400

    Examples Radio: add some unworkable examples

--- examples/ERadio.hs
+++ examples/ERadio.hs
@@ -11,10 +11,22 @@ import Kludges
 apiKey = APIKey "b25b959554ed76058ac220b7b2e0a026"
 
 tune :: APIKey -> SessionKey -> IO ()
-tune = undefined
+tune spiKey sessionKey = do response <- Radio.tune Nothing (Station "lastfm://artist/Merzbow/similarartists") apiKey sessionKey
+                            putStr "Tune: "
+                            case response of
+                              Left e  -> print e
+                              Right r -> print $ stationUrl r
+                            putStrLn ""
+  where stationUrl = getContent <=< lookupChild "url" <=< lookupChild "station" <=< wrap
 
 getPlaylist :: APIKey -> SessionKey -> IO ()
-getPlaylist = undefined
+getPlaylist apiKey sessionKey = do response <- Radio.getPlaylist Nothing Nothing Nothing (Multiplier 2.0) (Bitrate 64) apiKey sessionKey
+                                   putStr "Playlist: "
+                                   case response of
+                                     Left e  -> print e
+                                     Right r -> print $ titles r
+                                   putStrLn ""
+  where titles = mapM (getContent <=< lookupChild "title") <=< lookupChildren "track" <=< lookupChild "trackList" <=< lookupChild "playlist" <=< wrap
 
 search :: IO ()
 search = do response <- Radio.search (Name "dubstep") apiKey
