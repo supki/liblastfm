fbed445539eae1aa7279404f887f16ef788822b6
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-02-11 14:01:41 +0400

    Complete Chart module documentation.

--- Network/Lastfm/API/Chart.hs
+++ Network/Lastfm/API/Chart.hs
@@ -1,3 +1,5 @@
+-- | Chart API module
+{-# OPTIONS_HADDOCK prune #-}
 module Network.Lastfm.API.Chart
   ( getHypedArtists, getHypedTracks, getLovedTracks
   , getTopArtists, getTopTags, getTopTracks
@@ -6,21 +8,39 @@ module Network.Lastfm.API.Chart
 import Network.Lastfm.Response
 import Network.Lastfm.Types ((?<), APIKey, Limit, Page)
 
+-- | Get the hyped artists chart.
+--
+-- More: <http://www.lastfm.ru/api/show/chart.getHypedArtists>
 getHypedArtists :: Maybe Page -> Maybe Limit -> APIKey -> Lastfm Response
 getHypedArtists = get "getHypedArtists"
 
+-- | Get the hyped tracks chart.
+--
+-- More: <http://www.lastfm.ru/api/show/chart.getHypedTracks>
 getHypedTracks :: Maybe Page -> Maybe Limit -> APIKey -> Lastfm Response
 getHypedTracks = get "getHypedTracks"
 
+-- | Get the most loved tracks chart.
+--
+-- More: <http://www.lastfm.ru/api/show/chart.getLovedTracks>
 getLovedTracks :: Maybe Page -> Maybe Limit -> APIKey -> Lastfm Response
 getLovedTracks = get "getLovedTracks"
 
+-- | Get the top artists chart.
+--
+-- More: <http://www.lastfm.ru/api/show/chart.getTopArtists>
 getTopArtists :: Maybe Page -> Maybe Limit -> APIKey -> Lastfm Response
 getTopArtists = get "getTopArtists"
 
+-- | Get top tags chart.
+--
+-- More: <http://www.lastfm.ru/api/show/chart.getTopTags>
 getTopTags :: Maybe Page -> Maybe Limit -> APIKey -> Lastfm Response
 getTopTags = get "getTopTags"
 
+-- | Get the top tracks chart.
+--
+-- More: <http://www.lastfm.ru/api/show/chart.getTopTracks>
 getTopTracks :: Maybe Page -> Maybe Limit -> APIKey -> Lastfm Response
 getTopTracks = get "getTopTracks"
 
