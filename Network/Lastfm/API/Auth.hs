36135fd997a3a6172e98235934f41e090e643bf2
Dmitry Malikov <color404051h@gmail.com>
2012-02-15 00:01:27 +0400

    Complete refactoring

--- Network/Lastfm/API/Auth.hs
+++ Network/Lastfm/API/Auth.hs
@@ -15,7 +15,7 @@ import Network.Lastfm.Types ((?<), APIKey, AuthToken, SessionKey(..), Token(..),
 --
 -- More: <http://www.lastfm.ru/api/show/auth.getMobileSession>
 getMobileSession :: User -> APIKey -> AuthToken -> Lastfm Response
-getMobileSession user apiKey token = dispatch $ callAPI "auth.getMobileSession"
+getMobileSession user apiKey token = dispatch . callAPI "auth.getMobileSession" $
   [ "username" ?< user
   , "authToken" ?< token
   , "api_key" ?< apiKey
@@ -25,13 +25,13 @@ getMobileSession user apiKey token = dispatch $ callAPI "auth.getMobileSession"
 --
 -- More: <http://www.lastfm.ru/api/show/auth.getSession>
 getSession :: APIKey -> Token -> Lastfm Response
-getSession apiKey token = dispatch $ callAPI "auth.getSession" ["api_key" ?< apiKey, "token" ?< token]
+getSession apiKey token = dispatch . callAPI "auth.getSession" $ ["api_key" ?< apiKey, "token" ?< token]
 
 -- | Fetch an unathorized request token for an API account.
 --
 -- More: <http://www.lastfm.ru/api/show/auth.getToken>
 getToken :: APIKey -> Lastfm Response
-getToken apiKey = dispatch $ callAPI "auth.getToken" ["api_key" ?< apiKey]
+getToken apiKey = dispatch . callAPI "auth.getToken" $ ["api_key" ?< apiKey]
 
 -- | Construct the link to authorize token.
 getAuthorizeTokenLink :: APIKey -> Token -> String
