8650cc0fab85d76f98891e90144426e2b1db310d
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-06-10 02:32:18 +0400

    Fix some warnings. Enable -Werror ghc option in cabal file.

--- src/Network/Lastfm/API/Chart.hs
+++ src/Network/Lastfm/API/Chart.hs
@@ -5,16 +5,22 @@ module Network.Lastfm.API.Chart
 
 import Network.Lastfm.Internal
 
+getHypedArtists ∷ ResponseType → Maybe Page → Maybe Limit → APIKey → Lastfm Response
 getHypedArtists = get "getHypedArtists"
 
+getHypedTracks ∷ ResponseType → Maybe Page → Maybe Limit → APIKey → Lastfm Response
 getHypedTracks = get "getHypedTracks"
 
+getLovedTracks ∷ ResponseType → Maybe Page → Maybe Limit → APIKey → Lastfm Response
 getLovedTracks = get "getLovedTracks"
 
+getTopArtists ∷ ResponseType → Maybe Page → Maybe Limit → APIKey → Lastfm Response
 getTopArtists = get "getTopArtists"
 
+getTopTags ∷ ResponseType → Maybe Page → Maybe Limit → APIKey → Lastfm Response
 getTopTags = get "getTopTags"
 
+getTopTracks ∷ ResponseType → Maybe Page → Maybe Limit → APIKey → Lastfm Response
 getTopTracks = get "getTopTracks"
 
 get ∷ String → ResponseType → Maybe Page → Maybe Limit → APIKey → Lastfm Response
