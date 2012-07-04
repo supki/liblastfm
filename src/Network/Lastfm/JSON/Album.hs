8d58280016d0e6d836c66d19d9881c3b3b6c1859
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-07-05 00:17:05 +0400

    Add documentation gereration facility.

--- src/Network/Lastfm/JSON/Album.hs
+++ src/Network/Lastfm/JSON/Album.hs
@@ -6,52 +6,36 @@ module Network.Lastfm.JSON.Album
   , getTopTags, removeTag, search, share
   ) where
 
+#include "album.docs"
+
 import Network.Lastfm.Internal
 import qualified Network.Lastfm.API.Album as API
 
 $(json ["addTags", "getBuyLinks", "getInfo", "getShouts", "getTags", "getTopTags", "removeTag", "search", "share"])
 
--- | Tag an album using a list of user supplied tags.
---
--- More: <http://www.last.fm/api/show/album.addTags>
+__addTags__
 addTags ∷ (Artist, Album) → [Tag] → APIKey → SessionKey → Secret → Lastfm Response
 
--- | Get a list of Buy Links for a particular Album. It is required that you supply either the artist and track params or the mbid param.
---
--- More: <http://www.last.fm/api/show/album.getBuylinks>
+__getBuyLinks__
 getBuyLinks ∷ Either (Artist, Album) Mbid → Maybe Autocorrect → Country → APIKey → Lastfm Response
 
--- | Get the metadata for an album on Last.fm using the album name or a musicbrainz id. See playlist.fetch on how to get the album playlist.
---
--- More: <http://www.last.fm/api/show/album.getInfo>
+__getInfo__
 getInfo ∷ Either (Artist, Album) Mbid → Maybe Autocorrect → Maybe Language → Maybe Username → APIKey → Lastfm Response
 
--- | Get shouts for this album.
---
--- More: <http://www.last.fm/api/show/album.getShouts>
+__getShouts__
 getShouts ∷ Either (Artist, Album) Mbid → Maybe Autocorrect → Maybe Page → Maybe Limit → APIKey → Lastfm Response
 
--- | Get the tags applied by an individual user to an album on Last.fm.
---
--- More: <http://www.last.fm/api/show/album.getTags>
+__getTags__
 getTags ∷ Either (Artist, Album) Mbid → Maybe Autocorrect → Either User (SessionKey, Secret) → APIKey → Lastfm Response
 
--- | Get the top tags for an album on Last.fm, ordered by popularity.
---
--- More: <http://www.last.fm/api/show/album.getTopTags>
+__getTopTags__
 getTopTags ∷ Either (Artist, Album) Mbid → Maybe Autocorrect → APIKey → Lastfm Response
 
--- | Remove a user's tag from an album.
---
--- More: <http://www.last.fm/api/show/album.removeTag>
+__removeTag__
 removeTag ∷ Artist → Album → Tag → APIKey → SessionKey → Secret → Lastfm Response
 
--- | Search for an album by name. Returns album matches sorted by relevance.
---
--- More: <http://www.last.fm/api/show/album.search>
+__search__
 search ∷ Album → Maybe Page → Maybe Limit → APIKey → Lastfm Response
 
--- | Share an album with one or more Last.fm users or other friends.
---
--- More: <http://www.last.fm/api/show/album.share>
+__share__
 share ∷ Artist → Album → Recipient → Maybe Message → Maybe Public → APIKey → SessionKey → Secret → Lastfm Response
