bb8c77426ab13894ab6639586d72d9d907162f0b
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-07-05 15:14:58 +0400

    Move User modules to new docs facility.

--- src/Network/Lastfm/JSON/User.hs
+++ src/Network/Lastfm/JSON/User.hs
@@ -9,132 +9,84 @@ module Network.Lastfm.JSON.User
   , getWeeklyChartList, getWeeklyTrackChart, shout
   ) where
 
+#include "user.docs"
+
 import Network.Lastfm.Internal
 import qualified Network.Lastfm.API.User as API
 
 $(json ["getArtistTracks", "getBannedTracks", "getEvents", "getFriends", "getInfo", "getLovedTracks", "getNeighbours", "getNewReleases", "getPastEvents", "getPersonalTags", "getPlaylists", "getRecentStations", "getRecentTracks", "getRecommendedArtists", "getRecommendedEvents", "getShouts", "getTopAlbums", "getTopArtists", "getTopTags", "getTopTracks", "getWeeklyAlbumChart", "getWeeklyArtistChart", "getWeeklyChartList", "getWeeklyTrackChart", "shout"])
 
--- | Get a list of tracks by a given artist scrobbled by this user, including scrobble time. Can be limited to specific timeranges, defaults to all time.
---
--- More: <http://www.last.fm/api/show/user.getArtistTracks>
+__getArtistTracks__
 getArtistTracks ∷ User → Artist → Maybe StartTimestamp → Maybe EndTimestamp → Maybe Page → APIKey → Lastfm Response
 
--- | Returns the tracks banned by the user.
---
--- More: <http://www.last.fm/api/show/user.getBannedTracks>
+__getBannedTracks__
 getBannedTracks ∷ User → Maybe Page → Maybe Limit → APIKey → Lastfm Response
 
--- | Get a list of upcoming events that this user is attending.
---
--- Mpre: <http://www.last.fm/api/show/user.getEvents>
+__getEvents__
 getEvents ∷ User → Maybe Page → Maybe Limit → Maybe FestivalsOnly → APIKey → Lastfm Response
 
--- | Get a list of the user's friends on Last.fm.
---
--- More: <http://www.last.fm/api/show/user.getFriends>
+__getFriends__
 getFriends ∷ User → Maybe RecentTracks → Maybe Page → Maybe Limit → APIKey → Lastfm Response
 
--- | Get information about a user profile.
---
--- More: <http://www.last.fm/api/show/user.getInfo>
+__getInfo__
 getInfo ∷ Maybe User → APIKey → Lastfm Response
 
--- | Get tracks loved by a user.
---
--- More: <http://www.last.fm/api/show/user.getLovedTracks>
+__getLovedTracks__
 getLovedTracks ∷ User → Maybe Page → Maybe Limit → APIKey → Lastfm Response
 
--- | Get a list of a user's neighbours on Last.fm.
---
--- More: <http://www.last.fm/api/show/user.getNeighbours>
+__getNeighbours__
 getNeighbours ∷ User → Maybe Limit → APIKey → Lastfm Response
 
--- | Gets a list of forthcoming releases based on a user's musical taste.
---
--- More: <http://www.last.fm/api/show/user.getNewReleases>
+__getNewReleases__
 getNewReleases ∷ User → Maybe UseRecs → APIKey → Lastfm Response
 
--- | Get a paginated list of all events a user has attended in the past.
---
--- More: <http://www.last.fm/api/show/user.getPastEvents>
+__getPastEvents__
 getPastEvents ∷ User → Maybe Page → Maybe Limit → APIKey → Lastfm Response
 
--- | Get the user's personal tags.
---
--- More: <http://www.last.fm/api/show/user.getPersonalTags>
+__getPersonalTags__
 getPersonalTags ∷ User → Tag → TaggingType → Maybe Page → Maybe Limit → APIKey → Lastfm Response
 
--- | Get a list of a user's playlists on Last.fm.
---
--- More: <http://www.last.fm/api/show/user.getPlaylists>
+__getPlaylists__
 getPlaylists ∷ User → APIKey → Lastfm Response
 
--- | Get a list of the recent Stations listened to by this user.
---
--- More: <http://www.last.fm/api/show/user.getRecentStations>
+__getRecentStations__
 getRecentStations ∷ User → Maybe Page → Maybe Limit → APIKey → SessionKey → Secret → Lastfm Response
 
--- | Get a list of the recent tracks listened to by this user. Also includes the currently playing track with the nowplaying="true" attribute if the user is currently listening.
---
--- More: <http://www.last.fm/api/show/user.getRecentTracks>
+__getRecentTracks__
 getRecentTracks ∷ User → Maybe Page → Maybe Limit → Maybe From → Maybe To → APIKey → Lastfm Response
 
--- | Get Last.fm artist recommendations for a user.
---
--- Mpre: <http://www.last.fm/api/show/user.getRecommendedArtists>
+__getRecommendedArtists__
 getRecommendedArtists ∷ Maybe Page → Maybe Limit → APIKey → SessionKey → Secret → Lastfm Response
 
--- | Get a paginated list of all events recommended to a user by Last.fm, based on their listening profile.
---
--- More: <http://www.last.fm/api/show/user.getRecommendedEvents>
+__getRecommendedEvents__
 getRecommendedEvents ∷ Maybe Page → Maybe Limit → APIKey → SessionKey → Secret → Lastfm Response
 
--- | Get shouts for this user. Also available as an rss feed.
---
--- More: <http://www.last.fm/api/show/user.getShouts>
+__getShouts__
 getShouts ∷ User → Maybe Page → Maybe Limit → APIKey → Lastfm Response
 
--- | Get the top albums listened to by a user. You can stipulate a time period. Sends the overall chart by default.
---
--- More: <http://www.last.fm/api/show/user.getTopAlbums>
+__getTopAlbums__
 getTopAlbums ∷ User → Maybe Period → Maybe Page → Maybe Limit → APIKey → Lastfm Response
 
--- | Get the top artists listened to by a user. You can stipulate a time period. Sends the overall chart by default.
---
--- More: <http://www.last.fm/api/show/user.getTopArtists>
+__getTopArtists__
 getTopArtists ∷ User → Maybe Period → Maybe Page → Maybe Limit → APIKey → Lastfm Response
 
--- | Get the top tags used by this user.
---
--- More: <http://www.last.fm/api/show/user.getTopTags>
+__getTopTags__
 getTopTags ∷ User → Maybe Limit → APIKey → Lastfm Response
 
--- | Get the top tracks listened to by a user. You can stipulate a time period. Sends the overall chart by default.
---
--- More: <http://www.last.fm/api/show/user.getTopTracks>
+__getTopTracks__
 getTopTracks ∷ User → Maybe Period → Maybe Page → Maybe Limit → APIKey → Lastfm Response
 
--- | Get an album chart for a user profile, for a given date range. If no date range is supplied, it will return the most recent album chart for this user.
---
--- More: <http://www.last.fm/api/show/user.getWeeklyAlbumChart>
+__getWeeklyAlbumChart__
 getWeeklyAlbumChart ∷ User → Maybe From → Maybe To → APIKey → Lastfm Response
 
--- | Get an artist chart for a user profile, for a given date range. If no date range is supplied, it will return the most recent artist chart for this user.
---
--- More: <http://www.last.fm/api/show/user.getWeeklyArtistChart>
+__getWeeklyArtistChart__
 getWeeklyArtistChart ∷ User → Maybe From → Maybe To → APIKey → Lastfm Response
 
--- | Get a list of available charts for this user, expressed as date ranges which can be sent to the chart services.
---
--- More: <http://www.last.fm/api/show/user.getWeeklyChartList>
+__getWeeklyChartList__
 getWeeklyChartList ∷ User → APIKey → Lastfm Response
 
--- | Get a track chart for a user profile, for a given date range. If no date range is supplied, it will return the most recent track chart for this user.
---
--- More: <http://www.last.fm/api/show/user.getWeeklyTrackChart>
+__getWeeklyTrackChart__
 getWeeklyTrackChart ∷ User → Maybe From → Maybe To → APIKey → Lastfm Response
 
--- | Shout on this user's shoutbox.
---
--- More: <http://www.last.fm/api/show/user.shout>
+__shout__
 shout ∷ User → Message → APIKey → SessionKey → Secret → Lastfm Response
