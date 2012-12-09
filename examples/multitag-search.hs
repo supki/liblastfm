8d12396314eb5d40f3f09ad9704ef3ca761f882c
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-12-09 23:18:51 +0400

    Cosmetics.

--- examples/multitag-search.hs
+++ examples/multitag-search.hs
@@ -1,38 +1,40 @@
 #!/usr/bin/runhaskell
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE UnicodeSyntax     #-}
-{- This example shows how to multitag search could be kludged
+{- This example shows how to do multitag search even
+ - if it's not provided by Lastfm API
  -
  - Sample output:
  -
- - $> ./examples/multitag-search.hs "j-pop" "anime" "anime-ost"
+ - $> ./examples/multitag-search.hs
  - 坂本真綾
  - KOTOKO
  - Lisa
  - 茅原実里
  -}
-import           Control.Applicative ((<$>))
+import           Control.Applicative
 import           Control.Lens
 import           Data.Aeson.Lens
-import           Data.List           (intersect)
-import           Data.Text           (Text)
-import qualified Data.Text.IO        as T
-import           Data.Text.Lazy      (pack)
-import           System.Environment  (getArgs)
-
+import           Data.List (intersect)
+import           Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy.IO as T
 import           Network.Lastfm
-import           Network.Lastfm.Tag
+import qualified Network.Lastfm.Tag as Tag
 
 
-main ∷ IO ()
-main = cout =<< get_artists =<< read_args
-  where
-    cout = mapM_ T.putStrLn
-    read_args = Prelude.map Data.Text.Lazy.pack <$> getArgs
+main :: IO ()
+main = mapM_ T.putStrLn =<< artists multitags
 
 
-get_artists ∷ [Tag] → IO [Text]
-get_artists = (foldl1 intersect <$>) . mapM ((get_name <$>) . query)
-  where
-    query τ = lastfm $ getTopArtists τ <> limit 100 <> apiKey "29effec263316a1f8a97f753caaa83e0" <> json
-    get_name r = (r ^. key "topartists" . key "artist") ^.. folded . traverseArray .  key "name" . asText ^.. folded . folded
+artists :: [Tag] -> IO [Text]
+artists ts = do
+  names <- mapM (\t -> name <$> query t) ts
+  case names of
+    [] -> return []
+    ns -> return (foldl1 intersect ns)
+ where
+  query t = lastfm $ Tag.getTopArtists t <> limit 100 <> apiKey "29effec263316a1f8a97f753caaa83e0" <> json
+  name r = r ^. key "topartists" . key "artist" ^.. folded . traverseArray .  key "name" ^.. folded . folded
+
+
+multitags :: [Text]
+multitags = ["j-pop", "anime", "anime-ost"]
