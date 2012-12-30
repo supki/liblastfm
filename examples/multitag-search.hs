9f169cd550466f0709a5374cbf77c636898d29db
Dmitry Malikov <color404051h@gmail.com>
2012-12-31 03:58:11 +0400

    Rewrite multitag-search on pure aeson #9

--- examples/multitag-search.hs
+++ examples/multitag-search.hs
@@ -12,11 +12,11 @@
  - 茅原実里
  -}
 import           Control.Applicative
-import           Control.Lens
-import           Data.Aeson.Lens
+import           Data.Aeson.Types
 import           Data.List (intersect)
-import           Data.Text.Lazy (Text)
-import qualified Data.Text.Lazy.IO as T
+import           Data.Maybe
+import           Data.Text (Text)
+import qualified Data.Text.IO as T
 import           Network.Lastfm
 import qualified Network.Lastfm.Tag as Tag
 
@@ -27,15 +27,15 @@ main = mapM_ T.putStrLn =<< artists multitags
 
 artists :: [Text] -> IO [Text]
 artists ts = do
-  names <- mapM (\t -> name <$> query t) ts
+  names <- catMaybes <$> mapM (((parseMaybe gta =<<) <$>) . query) ts
   case names of
     [] -> return []
     ns -> return (foldl1 intersect ns)
  where
-  query t = lastfm $ Tag.getTopArtists <*> tag t <* limit 100
-    <*> apiKey "29effec263316a1f8a97f753caaa83e0" <* json
-  name r = r ^. key "topartists" . key "artist" ^.. folded . traverseArray .  key "name" ^.. folded . folded
+  query t = lastfm $ Tag.getTopArtists <*> tag t <* limit 100 <*> apiKey "29effec263316a1f8a97f753caaa83e0" <* json
 
+gta :: Value -> Parser [Text]
+gta o = parseJSON o >>= (.: "topartists") >>= (.: "artist") >>= mapM (.: "name")
 
 multitags :: [Text]
 multitags = ["j-pop", "anime", "anime-ost"]
