641292b77de3b27f9ebd6dc659fcf5015bdda179
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-01-02 01:41:00 +0400

    More docs.

--- src/Network/Lastfm.hs
+++ src/Network/Lastfm.hs
@@ -1,10 +1,13 @@
 module Network.Lastfm
-  ( -- * Control.Applicative is re-exported for convenience
-    module Control.Applicative
-  , module L
+  ( -- * Utilities for constructing requests
+    module Network.Lastfm.Request
+    -- * Utilities for signing and sending requests
+  , module Network.Lastfm.Response
+    -- * Control.Applicative is re-exported for convenience
+  , module Control.Applicative
   ) where
 
 import Control.Applicative
 
-import Network.Lastfm.Request as L hiding (get, post)
-import Network.Lastfm.Response as L
+import Network.Lastfm.Request
+import Network.Lastfm.Response
