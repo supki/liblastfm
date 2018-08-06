8603567a6bace48f44a0758e319302d4f248b798
Dmitry Malikov <malikov.d.y@gmail.com>
2018-08-07 00:10:21 +0200

    Make https a default scheme

--- src/Lastfm/Request.hs
+++ src/Lastfm/Request.hs
@@ -7,7 +7,7 @@ module Lastfm.Request
   ( -- * Request
     Request, R, Ready, Sign, Format(..)
     -- * Request major parameters
-  , api, https, post, get, json, xml, APIKey, apiKey, SessionKey, sessionKey
+  , api, post, get, json, xml, APIKey, apiKey, SessionKey, sessionKey
     -- * Request minor parameters
   , Token, token, Callback, callback
   , Artist, artist, artists, Album, album, MBID, mbid
@@ -76,10 +76,6 @@ data Callback
 -- Primarily used in API call wrappers, not intended for usage by library user
 api :: Text -> Request f a
 api = add "method"
-
--- | Change API scheme to https ('http' is the default one)
-https :: Request f a
-https = wrap $ \r -> r { _host = "https://ws.audioscrobbler.com/2.0/" }
 
 -- | Change html _method to GET
 --
